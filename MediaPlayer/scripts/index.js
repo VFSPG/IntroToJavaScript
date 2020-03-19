bc957e0e740f6581cad97564327df87e26b73499
KIBBLEONE\Scott <scott@kibblegames.com>
2020-03-19 15:05:04 -0700

    Adding functions and objects and other looping stuff

--- MediaPlayer/scripts/index.js
+++ MediaPlayer/scripts/index.js
@@ -7,6 +7,78 @@
 
 import App from './App.js';
 
+let thePlayList = [];
+
+class Track {
+
+    constructor( songTitle, album, mediaUri ) {
+
+        this.songTitle = songTitle;
+        this.albumImage = "url/album/image.jpg";
+        this.name = "Said the Whale";
+        this.album = "Mother";
+        this.year = "2013";
+        this.duration = "00:00";
+        this.media = "../songdata/mother.mp3";
+    }
+}
+
+let myTrack = new Track("Mother", "Mother", "../songdata/mother.mp3");
+let theSecondTrack = new Track("Paralyzer", "???", "../songdata/paralyser.mp3");
+
+let track = {
+    songTitle:"Mother",
+    albumImage: "url/album/image.jpg",
+    name: "Said the Whale",
+    album: "Mother",
+    year: "2013",
+    duration: "00:00",
+    media: "../songdata/mother.mp3",
+}
+
+const MAX_SONGS = 50;
+
+for (let i = 0; i < MAX_SONGS; i++) {
+
+    thePlayList[i] = { ...track };
+}
+
+//for (let j = 0; j < thePlayList.length; j++) {
+for ( let j in thePlayList) {
+//for (let currentSong of thePlayList) {
+    let currentSong = thePlayList[ j ];
+    // do some magic with currentSong to play it in HTML5
+    playCurrentSong( currentSong );
+}
+
+let i = 0;
+let done = false;
+let playing = false;
+while (!done && !playing) {
+
+    if (!playing) {
+
+        let currentSong = thePlayList[i++];
+    }
+    playing = true;
+    playCurrentSong( currentSong );
+}
+
+
+function playCurrentSong( theTrack ) {
+
+    // go get some code from a library to look up the uri and play the mp3
+    return currentDuration( theTrack );
+}
+
+function currentDuration( theTrack ) {
+
+    let currentDuration = theTrack.duration - theTrack.progress;
+    return currentDuration;
+}
+
+
+
 (function Main() {
 
     // Wait for the DOM to finish loading (we don't want to reference things that don't exist)
