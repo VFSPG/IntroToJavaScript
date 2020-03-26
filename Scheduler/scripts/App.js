54cc595ddd7e3a30934574c5b6ab534d61b6c281
KIBBLEONE\Scott <scott@kibblegames.com>
2020-03-26 16:35:14 -0700

    Class updates - integrating HTML and JavaScript

--- Scheduler/scripts/App.js
+++ Scheduler/scripts/App.js
@@ -15,18 +15,57 @@ export default class App {
 
 	constructor() {
 
+        /*
+        <table>
+            <tbody>
+                <tr><td></td><td>Monday</td><td>Tuesday</td><td>Wednesday</td><td>Thursday</td><td>Friday</td></tr>
+                <tr><td>AM</td><td id="monday-am"></td><td></td><td></td><td></td><td></td></tr>
+                <tr><td>PM</td><td></td><td></td><td></td><td></td><td></td></tr>
+                <tr><td>EVE</td><td></td><td></td><td></td><td></td><td></td></tr>
+            </tbody>
+        </table>
+        */
+        let daysOfTheWeek = ['sunday','monday','tuesday','wednesday','thursday','friday'];
+        let el = document.querySelector("#schedule-grid");
+
+        let markup = "<table>";   // ""  | ''  | ``
+        //let markup = '<table>';
+        //let markup = `<table>`;
+        markup += "<tr><td></td>";
+        for (let day of daysOfTheWeek) {
+            markup += `<td id="${day}">${day}</td>`;
+        }
+        markup += "</tr>";
+        for (let timeslot of ['am','pm','eve']) {
+            markup += `<tr><td>${timeslot}</td>`;
+
+            for (let day of daysOfTheWeek) {
+
+                markup += `<td id="${day}-${timeslot}"></td>`;
+            }
+            markup += "</tr>";
+        }
+        markup += "</tbody></table>";
+        el.innerHTML = markup;
+
         // Initialize any app variables or code we will need
         this.mySchedule = new Schedule();
-        let javaScriptCourse = new Course('JavaScript & jQuery', AM, "Thursaday");
-        let designInterface = new Course('Interface Design', EVE, "Wednesday");
+        let javaScriptCourse = new Course('JavaScript & jQuery', 0, "Thursaday");
+        let designInterface = new Course('Interface Design', 2, "Wednesday");
 
         javaScriptCourse.setLocation("Home");
         javaScriptCourse.assignInstructor("Scott");
-
+/*
         let scottsSchedule = generateEmptySchedule();
         let mardiasSchedule = generateEmptySchedule();
-
+*/
         // Initialize any event handlers
+    }
+
+    add( p1, p2 = 0 ) {
+
+        let temp = p1 + p2;
+        return temp;
     }
 
 	run() {
