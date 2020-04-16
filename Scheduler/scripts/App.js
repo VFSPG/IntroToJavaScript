102f6de5533481aa4a1cbafa009de5816b7bd21c
Savannah <leishmansavannah@gmail.com>
2020-04-16 02:36:25 -0700

    - first and last commit - formatting and styling

--- Scheduler/scripts/App.js
+++ Scheduler/scripts/App.js
@@ -1,23 +1,80 @@
 /**
  * @copyright: (C) 2020 Vancouver Film School.  All Rights Reserved.
- * @author:    Your Name {@link mailto:ddXXname@vfs.com}
+ * @author:    Your Name {@link savannah:dd47savannah@vfs.com}
  * @version:   1.0
  */
 'use strict';
 
-// export ... = something that can be imported to another module
-// ... default = the default thing to export can be imported without {}
-//      ... class = an Object definition (not an instance of an Object), we can make more
 export default class App {
 
-	constructor() {
+  	constructor() {
+        this.daysOfTheWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday','Friday'];
+        document.querySelector("#schedule-grid").innerHTML = this.generateSchedule(this.daysOfTheWeek);
+        this.eventMessage = "add button handled"
 
-        // Initialize any app variables or code we will need
+        this.todoItem = 0;
 
-        // Initialize any event handlers
+        document.querySelector("#todo-add-item-button").addEventListener("click", (event) => {
+            //get the value of the text input
+                this.addTodoItem(event);
+
+            this.todoItem++;
+
+            document.querySelector("#virtual-console").innerHTML = this.eventMessage;
+        })
     }
 
-	run() {
-        // just waiting around for something to happen...
+    addTodoItem(event){
+
+            let todoText = document.querySelector('input[name="new-item"]').value;
+
+            let markup =`<li id="item-${this.todoItem}">`;
+            markup +=           `<input type = "checkbox" name= "item-done" value="false">`;
+            markup +=           `<span class = "todo-text">${todoText}</span>`;
+            markup +=    `</li>`;
+
+            let listItem = document.querySelector(".todo-list");
+            let currentElements = listItem.innerHTML;
+            currentElements += markup;
+            listItem.innerHTML = currentElements;
+    }
+
+    generateSchedule(daysOfTheWeek){
+            // the code below builds this............ woooooo! 
+            // <table>
+               //<tbody>
+                    //<tr><td></td><td>Monday</td><td>Tuesday</td><td>Wednesday</td><td>Thursday</td><td>Friday</td>></tr>
+                    //<tr><td>AM</td><td id="monday-am"></td><td></td><td></td><td></td><td></td></tr>
+                    //<tr><td>PM</td><td></td><td></td><td></td><td></td><td></tr>
+                    //<tr><td>EVE</td><td></td><td></td><td></td><td></td><td></tr>
+                //</tbody>
+            // </table>
+        
+            let markup = "<table>"; 
+            markup += "<tr><td></td>";
+            for (let day of daysOfTheWeek) {
+              markup += `<td id="${day}">${day}</td>`;
+            }
+            markup += "</tr>";
+
+            for  (let timeslot of ['AM','PM','EVE']) {
+                markup += this.generateScheduleRow( daysOfTheWeek , timeslot );
+            }
+            markup +="</tbody></table>";
+            return markup;
+    }
+
+    generateScheduleRow( daysOfTheWeek, timeslot ){
+        let markup = `<tr><td>${timeslot}</td>`;
+        for (let day of daysOfTheWeek) {
+            markup += `<td id="${day}-${timeslot}"></td>`;
+        }
+        markup += "</tr>";
+        return markup;
+    }
+
+    run() {
+            // just waiting around for something to happen...
     }
 }
+
